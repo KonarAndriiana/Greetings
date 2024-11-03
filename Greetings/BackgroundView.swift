05c6b9bee73a53c115f6b89ba1070835c9c9b8e4
KonarAndriiana <andriiana.konar@kosickaakademia.sk>
2024-11-03 21:33:27 +0100

    Update BackgroundView and Launch Screen storyboard for improved design and color handling

--- Greetings/BackgroundView.swift
+++ Greetings/BackgroundView.swift
@@ -6,7 +6,7 @@
 //
 
 import SwiftUI
-
+///This is background 
 struct BackgroundView: View {
     var body: some View {
         LinearGradient(colors: [Color.blue1, Color.blue2] , startPoint: .topLeading, endPoint: .bottomTrailing).opacity(1).ignoresSafeArea()
