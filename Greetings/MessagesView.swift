ba4877b930c35dbed78a91251fa135ec93c208bb
KonarAndriiana <andriiana.konar@kosickaakademia.sk>
2024-11-03 17:28:19 +0100

    Add MessagesView and TitleView components

---
+++ Greetings/MessagesView.swift
@@ -0,0 +1,18 @@
+//
+//  MessagesView.swift
+//  Greetings
+//
+//  Created by Andriiana Konar on 03/11/2024.
+//
+
+import SwiftUI
+
+struct MessagesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MessagesView()
+}
