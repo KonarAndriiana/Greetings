affaa7e910a558093252e77723bf3e0f21009533
KonarAndriiana <andriiana.konar@kosickaakademia.sk>
2024-11-03 17:23:45 +0100

    Add greeting title and subtitle to ContentView

--- Greetings/ContentView.swift
+++ Greetings/ContentView.swift
@@ -13,18 +13,30 @@ struct ContentView: View {
         .init(text: "Welcome to SWIFT Programing", color: .gray),
         .init(text: "Are you ready to , ", color: .yellow),
         .init(text: "start expoliring ?", color: .red),
-        .init(text: "Boom.", color: .purple),
-        .init(text: "Let's go !", color: .blue),
-        .init(text: "Let's go !", color: .blue)
+        .init(text: "Boom.", color: .purple)
     ]
     
     var body: some View {
         VStack(alignment: .leading) {
             
+            //Title
+            VStack(alignment: .leading , spacing: 0){
+                Text("Greetings")
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+                Text("Explore IOS Programming")
+                    .font(.headline)
+                    .fontWeight(.thin)
+            }
+            
+            Spacer()
+            
             ForEach(messages) {
                 dataItem in TextView(text: dataItem.text, color: dataItem.color)
             }
             
+            Spacer()
+            Spacer()
         }
         .padding()
     }
