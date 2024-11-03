894a8c56072158d50012815c42bc3044dccd3ceb
KonarAndriiana <andriiana.konar@kosickaakademia.sk>
2024-11-03 14:14:44 +0100

    Initial Commit

---
+++ Greetings/GreetingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreetingsApp.swift
+//  Greetings
+//
+//  Created by Andriiana Konar on 03/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct GreetingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
