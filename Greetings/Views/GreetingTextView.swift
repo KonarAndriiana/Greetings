e61f838122804f18011e345a29a8d3544c74e650
KonarAndriiana <andriiana.konar@kosickaakademia.sk>
2024-11-07 18:45:21 +0100

    Add GreetingTextView and RotatableCircleView for new UI components

---
+++ Greetings/Views/GreetingTextView.swift
@@ -0,0 +1,18 @@
+//
+//  GreetingTextView.swift
+//  Greetings
+//
+//  Created by Andriiana Konar on 07/11/2024.
+//
+
+import SwiftUI
+
+struct GreetingTextView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GreetingTextView()
+}
---
+++ Greetings/Views/RotatableCircleView.swift
@@ -0,0 +1,18 @@
+//
+//  RotatableCircleView.swift
+//  Greetings
+//
+//  Created by Andriiana Konar on 07/11/2024.
+//
+
+import SwiftUI
+
+struct RotatableCircleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RotatableCircleView()
+}
