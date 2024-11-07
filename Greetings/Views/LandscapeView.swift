66b5601dfdb28893714b5953a18b6014cf0b0f1a
KonarAndriiana <andriiana.konar@kosickaakademia.sk>
2024-11-07 17:07:03 +0100

    Update supported interface orientations and add LandscapeView for improved layout support

---
+++ Greetings/Views/LandscapeView.swift
@@ -0,0 +1,33 @@
+//
+//  LandscapeView.swift
+//  Greetings
+//
+//  Created by Andriiana Konar on 07/11/2024.
+//
+
+import SwiftUI
+
+struct LandscapeView: View {
+    var body: some View {
+        ZStack {
+            
+            BackgroundView()
+            
+            VStack(alignment: .leading) {
+                TitleView()
+                
+                Spacer()
+                
+                MessagesView()
+                
+                Spacer()
+                Spacer()
+            }
+            .padding()
+        }
+    }
+}
+
+#Preview {
+    LandscapeView()
+}
